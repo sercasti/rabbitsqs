c48429b7d91372fc6a51e79834db9d09e6e08c24
Sergio Castiñeyras <scastine@amazon.com>
2022-08-26 17:11:02 -0300

    initisl producer and consumer with rabbit

--- code/consumer.py
+++ code/consumer.py
@@ -7,11 +7,15 @@ import json
 # get Environment Variables
 RABBIT_HOST = os.environ['mqhost']
 RABBIT_USER = os.environ['mquser']
-RABBIT_PWD = os.environ['mquser']
+RABBIT_PWD = os.environ['mqpass']
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
+ssl_options = pika.SSLOptions(context=ssl_context)
 
 credentials = pika.PlainCredentials(RABBIT_USER, RABBIT_PWD)
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-parameters = pika.ConnectionParameters(credentials=credentials, ssl=True, host=RABBIT_HOST, virtual_host=RABBIT_USER)  
+parameters = pika.ConnectionParameters(credentials=credentials, host=RABBIT_HOST, ssl_options=ssl_options)  
 
 # parameters and credentials ready to support calls to RabbitMQ
 connection = pika.BlockingConnection(parameters)  #Establishes TCP Connection with RabbitMQ
